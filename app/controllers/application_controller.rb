b2bc4acc41c1514d69aae3e24ecc1879d071b077
Tomoyuki Ekuni <tekuni@yahoo.com>
2018-11-07 17:21:37 +0900

    completed the second exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '19094569004',
+    category:     'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '19099691532',
+    category:     'chinese'
+  },
+  {
+    name:         'Mos Burger',
+    address:      'Tokyo',
+    phone_number: '13243534433',
+    category:     'belgian'
+  },
+  {
+    name:         'Of Course',
+    address:      'Paris',
+    phone_number: '64564524234',
+    category:     'french'
+  },
+  {
+    name:         'Sushi Stop',
+    address:      'Meguro',
+    phone_number: '3324234234',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
